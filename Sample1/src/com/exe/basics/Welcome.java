7b20bb8e6aedeffb044abe3eed75a7294158ea55
phani <phanikumar.wd@gmail.com>
2019-05-13 14:54:22 -0400

    Sample Project

---
+++ Sample1/src/com/exe/basics/Welcome.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.exe.basics;
+
+/**
+ * @author SCARTO
+ *
+ */
+public class Welcome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		boolean flag = true;
+
+		System.out.println("Hello");
+		if (flag) {
+
+			System.out.println("If Block");
+		} else {
+			System.out.println("Else Block");
+
+		}
+	}
+
+}
