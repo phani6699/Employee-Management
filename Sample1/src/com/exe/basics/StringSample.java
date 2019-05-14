2981821e9b6dbd4d7696977394abe3a0ac709664
phani <phanikumar.wd@gmail.com>
2019-05-14 18:44:02 -0400

    Sample String

---
+++ Sample1/src/com/exe/basics/StringSample.java
@@ -0,0 +1,18 @@
+package com.exe.basics;
+
+public class StringSample {
+
+	public static void main(String[] args) {
+		String s = "Welcome";
+		String a = "World";
+		System.out.println(s);
+		System.out.println(s.charAt(1));
+		System.out.println(s.toUpperCase());
+		System.out.println(s.toLowerCase());
+		System.out.println(s.substring(2));
+		System.out.println(s.substring(2, 3));
+		System.out.println(s + ">" + a);
+
+	}
+
+}
